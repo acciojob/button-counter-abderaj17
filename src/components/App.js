03bd8555f136204e5da5ba6e168100d749faece0
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-09-14 22:01:32 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ const App = () => {
   const[count, setCount] = useState(0)
   return (
     <div>
-        {<p>Button Clicked {count} times</p>}
-        {<button onClick={()=>{
-          setCount(count+1);
-        }}>Click me</button>}
+        <p>Button Clicked {count} times</p>
+        <button onClick={()=>{
+          setCount(count+1)
+        }}>Click me</button>
     </div>
   )
 }
